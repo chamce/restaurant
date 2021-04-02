e90f12a793a625b07826ad4b45d3eb77f5161ff6
chamce <whitakerchancellor@yahoo.com>
2021-04-02 10:07:15 -0400

    12 appetizers

--- src/Section.js
+++ src/Section.js
@@ -6,8 +6,11 @@ export default function Section(props) {
     let n = section.id * 10;
     return (
         <>
+            {/* section name */}
             <h2>{props.section.name}</h2>
+            {/* section is an accordion; each accordion needs a unique id */}
             <div className="accordion accordion-flush" id={"accordionFlushParent" + n}>
+                {/* map all of section's items to item components; send item's parent id and make item's id unique to parent */}
                 {section.items.map((item, index) => <Item item={item} parentNumber={n} number={n + index} key={index}></Item>)}
             </div>
         </>
